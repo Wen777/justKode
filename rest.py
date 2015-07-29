2aa68abddeeab629f0a09e5b64899b504ba4c0ad
wen777 <shih777577@gmail.com>
2015-07-29 20:58:42 +0800

    about #2, create container

--- rest.py
+++ rest.py
@@ -1,9 +1,32 @@
+from pymongo import MongoClient
 from flask import Flask
+from docker import Client 
+
+cli = Client(base_url='unix://var/run/docker.sock')
+monCli = MongoClient('mongodb://localhost:27017/')
+jobDB = monCli['job']
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/ping")
 def ping():
-    return "python server received 'ping' request!"
+    msg = ""
+    jobCollection = jobDB.job
+    jobData = jobCollection.find_one({ "$query":{}, "$orderby":{'createAt':-1} })
+
+    if jobData != None:
+        msg = runContainer(jobData)
+    if msg != "":
+        return "python server received 'ping' request!"
+    else:
+        assert "raise error, msg is empty string."
+        pass
+
+def runContainer(jobData):
+    container = cli.create_container(image=jobData['image'])
+    #FIXME update job status
+    print(container)
+    return "Done"
 
 if __name__ == "__main__":
     app.run(debug=True)
